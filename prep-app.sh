2d6e85587b7ac3ee0fec241ba9a3e1f8ad73cc07
Collin Miller <collintmiller@gmail.com>
2020-09-22 13:33:27 -0500

    split tasks

--- prep-app.sh
+++ prep-app.sh
@@ -1,3 +1,4 @@
-docker-compose -f $1 run web bundle exec rails db:drop db:create db:migrate db:seed
+docker-compose -f $1 run web bundle exec rails db:drop db:create db:migrate
+docker-compose -f $1 run web bundle exec rails db:seed
 docker-compose -f $1 run web bundle exec rake spree_sample:load
-docker-compose -f $1 run web bundle exec rails g spree:frontend:copy_storefront
+# docker-compose -f $1 run web bundle exec rails g spree:frontend:copy_storefront
