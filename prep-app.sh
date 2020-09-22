c04d439954744d2f47846266759fc7b9eee3953d
Collin Miller <collintmiller@gmail.com>
2020-09-22 13:05:19 -0500

    wipe database when prepping

--- prep-app.sh
+++ prep-app.sh
@@ -1,6 +1,3 @@
-docker-compose -f $1 run web bundle exec rails db:create
-docker-compose -f $1 run web bundle exec rails db:migrate
-docker-compose -f $1 run web bundle exec rails db:seed
+docker-compose -f $1 run web bundle exec rails db:drop db:create db:migrate db:seed
 docker-compose -f $1 run web bundle exec rake spree_sample:load
 docker-compose -f $1 run web bundle exec rails g spree:frontend:copy_storefront
-# docker-compose -f $1 run web bundle exec rails assets:precompile
