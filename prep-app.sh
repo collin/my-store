6138d3b91100f4aeeb30680baa12066f14fd8fa4
Collin Miller <collintmiller@gmail.com>
2020-09-22 13:44:25 -0500

    allow destructive db action in production

--- prep-app.sh
+++ prep-app.sh
@@ -1,4 +1,4 @@
-docker-compose -f $1 run web bundle exec rails db:drop db:create db:migrate
+docker-compose -f $1 run web DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop db:create db:migrate
 docker-compose -f $1 run web bundle exec rails db:seed
 docker-compose -f $1 run web bundle exec rake spree_sample:load
 # docker-compose -f $1 run web bundle exec rails g spree:frontend:copy_storefront
